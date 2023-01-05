fcee87c75de5a9c5002eb87614de92503eab1677
Bartosz Elert <bartoszelert@Bartoszs-MacBook-Pro.local>
2023-01-05 23:25:11 +0100

    added geolocation hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useEffect } from "react";
+import useGeolocation from "../hooks/useGeolocation/useGeolocation";
 
 const Home: NextPage = () => {
+  const { position, getPosition } = useGeolocation();
+
+  useEffect(() => {
+    getPosition();
+  }, [getPosition]);
+
   return (
     <>
       <Head>
@@ -10,7 +18,24 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-[hsl(0,65%,40%)] to-[hsl(225,83%,40%)]">
-        <h1 className="text-6xl font-extrabold text-stone-200">Tempest</h1>
+        <header className="flex flex-col gap-10">
+          <h1 className="text-6xl font-extrabold text-stone-200">Tempest</h1>
+          {position && (
+            <div className="flex flex-col gap-1">
+              <h2 className="text-2xl font-medium text-stone-300">
+                Your position
+              </h2>
+              <div>
+                <p className="text-lg font-medium text-stone-400">
+                  Latitude: {position?.coords.latitude}
+                </p>
+                <p className="text-lg font-medium text-stone-400">
+                  Longitude: {position?.coords.longitude}
+                </p>
+              </div>
+            </div>
+          )}
+        </header>
       </main>
     </>
   );
