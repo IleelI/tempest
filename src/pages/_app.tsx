e6db59d8d2090af33b0361f4036a8dc933b78c07
Bartosz Elert <bartoszelert@Bartoszs-MacBook-Pro.local>
2023-01-07 18:02:04 +0100

    fixed invalid files structure inside pages directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { DarkModeProvider } from "../context/darkModeContext";
-import Container from "./_container";
+import {
+  DarkModeProvider,
+  useDarkModeContext,
+} from "../context/darkModeContext";
+import type { AppProps, AppType } from "next/app";
+import { Quicksand } from "@next/font/google";
+import clsx from "clsx";
 
 const queryClient = new QueryClient();
 
@@ -18,3 +21,20 @@ const MyApp: AppType = (appProps) => {
 };
 
 export default MyApp;
+
+const mainFont = Quicksand({
+  weight: "variable",
+  subsets: ["latin", "latin-ext"],
+  variable: "--font-main",
+});
+function Container({ Component, pageProps }: AppProps) {
+  const { isDarkMode } = useDarkModeContext();
+
+  return (
+    <div
+      className={clsx([mainFont.variable, "font-sans", isDarkMode && "dark"])}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
+}
